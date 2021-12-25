dc2fd0319611e6deffa239e0abc7c30485754e92
Julio Santos <julio.1810478300077@faeterj-rio.edu.br>
2021-12-25 19:25:58 -0300

    Add files via upload

---
+++ alterarSalaCandidato.php
@@ -0,0 +1,24 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  include 'conexao.php';
+
+  $id = $_GET["id"];
+  $cpf = $_GET["cpf"];
+  $nome = $_GET["nome"];
+  $email = $_GET["email"];
+  $sala = $_GET["sala"];
+  $salaN = $_GET["salaN"];
+  
+  
+ $sql = "UPDATE candidatos SET `sala`='$salaN' WHERE `id`=$id";
+  
+
+  $result = $conn->query($sql);
+
+  if($result) {
+    echo "Sala do candidato $nome alterada com sucesso para $salaN.";
+  } else {
+      echo "Erro ao alterar Produto.";
+    }  
+}
+?>
